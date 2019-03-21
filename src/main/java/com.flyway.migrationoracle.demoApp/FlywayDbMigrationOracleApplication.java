26ef6cbe70ed4a4c3527721025b0661c1f6390f4
e068635 <elson.franca@mastercard.com>
2019-03-20 22:20:51 -0300

    initial version

---
+++ src/main/java/com.flyway.migrationoracle.demoApp/FlywayDbMigrationOracleApplication.java
@@ -0,0 +1,14 @@
+package com.flyway.migrationoracle.demoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FlywayDbMigrationOracleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayDbMigrationOracleApplication.class, args);
+    }
+
+}
+
