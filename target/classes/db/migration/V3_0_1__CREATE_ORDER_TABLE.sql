fe29f74b645298c96e40f05cdb0f81785902e49b
e068635 <elson.franca@mastercard.com>
2019-04-05 15:10:21 -0300

    added create order table migrations

---
+++ target/classes/db/migration/V3_0_1__CREATE_ORDER_TABLE.sql
@@ -0,0 +1,31 @@
+--Story: S000002 | 1.0 | [DB PREP] CREATE_ORDER
+--Description: Create a new cart at the FLYWAYUSER.ORDER.
+--Author: Elsao boladao
+--Date: 2019-04-03
+--Error code reference: https://docs.oracle.com/cd/B10500_01/server.920/a96525/e900.htm
+
+ALTER SESSION SET DDL_LOCK_TIMEOUT = 60;
+
+DECLARE
+  COUNT_TABLE_ORDER PLS_INTEGER;
+  COUNT_CART_ID PLS_INTEGER;
+  COUNT_COMMENT_CART_ID PLS_INTEGER;
+
+  V_TABLE_NAME VARCHAR2(30) := 'ORDER';
+  V_SCHEMA_NAME VARCHAR2(30) := 'FLYWAYUSER';
+
+  BEGIN
+     SELECT COUNT(*) INTO COUNT_TABLE_CART FROM ALL_TABLES WHERE TABLE_NAME = V_TABLE_NAME AND OWNER = V_SCHEMA_NAME;
+
+     IF(COUNT_TABLE_CART = 0) THEN
+       BEGIN
+			   EXECUTE IMMEDIATE 'CREATE TABLE ' || V_SCHEMA_NAME || '.' || V_TABLE_NAME || '( ID NUMBER (10,0) NOT NULL, CONSTRAINT ORDER_ID_PK PRIMARY KEY(ID))';
+
+			   EXCEPTION WHEN OTHERS THEN
+			       IF SQLCODE != -00955 -- Name already used by an existing object (For more details refer to error code described at header);
+				      THEN RAISE;
+				     END IF;
+		   END;
+	  END IF;
+  END;
+/
